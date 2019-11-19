c8e5236fe13e654aeb0bc9b820495b5595dfa568
rzeczuchy <>
2019-11-19 23:14:00 +0100

    basic layout, displaying ToDo list, adding ToDos

---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rzeczuchyToDo2
+{
+    public partial class MainWindow : Window
+    {
+        private Stack<ToDo> todos;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            todos = new Stack<ToDo>();
+            todos.Push(new ToDo("this is a checked ToDo", true));
+            todos.Push(new ToDo("this is an unchecked ToDo", false));
+            for (int i = 0; i < 100; i++)
+            {
+                todos.Push(new ToDo("this is a checked ToDo", true));
+                todos.Push(new ToDo("this is an unchecked ToDo", false));
+            }
+            NewToDoTextBox.MaxLength = ToDo.MaxNameLenght;
+
+            DisplayToDoList();
+        }
+
+        private void DisplayToDoList()
+        {
+            if (todos != null)
+            {
+                ToDoListView.ItemsSource = todos;
+            }
+        }
+
+        private void AddToDoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewToDoTextBox.Text != "")
+            {
+                todos.Push(new ToDo(NewToDoTextBox.Text, false));
+                ToDoListView.Items.Refresh();
+            }
+            NewToDoTextBox.Text = "";
+        }
+    }
+}
---
+++ Source/ToDo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rzeczuchyToDo2
+{
+    class ToDo
+    {
+        public const int MaxNameLenght = 50;
+
+        public ToDo(string name, bool isChecked)
+        {
+            Name = name.Length > MaxNameLenght ? name.Substring(0, MaxNameLenght) : name;
+            IsChecked = isChecked;
+        }
+
+        public string Name { get; private set; }
+        public bool IsChecked { get; private set; }
+
+        public void CheckUncheck()
+        {
+            IsChecked = !IsChecked;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
